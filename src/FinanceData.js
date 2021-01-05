c3e31f272e7fb36c865a040929d5141ae33a8cdb
Landon Whitesides <whitesideslandon@gmail.com>
2021-01-04 23:14:47 -0700

    initial app completed

--- src/FinanceData.js
+++ src/FinanceData.js
@@ -7,53 +7,71 @@ import FinanceForm from "./FinanceForm";
 const FinanceData = () => {
 
 
-  const [symbol, setSymbol] = useState("IMMR")
+  const [symbol, setSymbol] = useState("AAPL")
+  const [companyName, setCompanyName] = useState("Apple Computers")
+  const [revenue, setRevenue] = useState(0)
+  const [profit, setProfit] = useState(0)
+  const [userInput, setUserInput] = useState(false)
 
   const options = {
     method: 'GET',
     url: 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-statistics',
     params: {symbol: symbol, region: 'US'},
     headers: {
-      'x-rapidapi-key': 'a9ca5c9906msh7dfe4547c325051p1692e6jsn798668ee52bb',
+      'x-rapidapi-key': process.env.REACT_APP_API_KEY,
       'x-rapidapi-host': 'apidojo-yahoo-finance-v1.p.rapidapi.com'
     }
   };
 
   
-  const [revenue, setRevenue] = useState(0)
-  const [profit, setProfit] = useState(0)
+
 
   useEffect(()=>{
+
+    if (userInput === false) {
     axios.request(options).then(function (response) {
       console.log(response.data);
       setSymbol(response.data.symbol)
       setRevenue(response.data.financialData.totalRevenue.raw)
       setProfit(response.data.financialData.grossProfits.raw)
     }).catch(function (error) {
+      setSymbol("AAPL")
+      setCompanyName("Apple Computers")
+      setRevenue(274515001344)
+      setProfit(104956000000)
       console.error(error);
     });
+  }
   },[])
 
-  const setCompanySymbol=(symbol)=>{
+  const setCompanySymbol=(symbol,companyName)=>{
+    setUserInput(true)
     setSymbol(symbol)
+    setCompanyName(companyName)
+    // debugger
     axios.request(options).then(function (response) {
       console.log(response.data);
-      setSymbol(response.data.symbol)
+      // setSymbol(response.data.symbol)
       setRevenue(response.data.financialData.totalRevenue.raw)
       setProfit(response.data.financialData.grossProfits.raw)
     }).catch(function (error) {
+      setSymbol("AAPL")
+      setCompanyName("Apple Computers")
+      setRevenue(274515001344)
+      setProfit(104956000000)
       console.error(error);
+      
     });
    
   }
 
 return (
   <Container>
-    <h1> 
-      Finance Data for {symbol}
-      <FinanceForm/>
-      <FinanceChart symbol = {symbol} revenue={revenue} profit={profit} setCompanySymbol={setCompanySymbol}/>
-    </h1>
+    <h1> Finance Data for {symbol}</h1>
+    <h3>{companyName}</h3>
+      <FinanceForm setCompanySymbol={setCompanySymbol}/>
+      <FinanceChart symbol = {symbol} revenue={revenue} profit={profit} />
+    
     </Container>
 )
 
--- src/FinanceForm.js
+++ src/FinanceForm.js
@@ -13,7 +13,7 @@ const FinanceForm = (props) => {
     url: 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete',
     params: {q: `${company}`, region: 'US'},
     headers: {
-      'x-rapidapi-key': 'a9ca5c9906msh7dfe4547c325051p1692e6jsn798668ee52bb',
+      'x-rapidapi-key': process.env.REACT_APP_API_KEY,
       'x-rapidapi-host': 'apidojo-yahoo-finance-v1.p.rapidapi.com'
     }
   };
@@ -24,8 +24,10 @@ const FinanceForm = (props) => {
     axios.request(options).then(function (response) {
       console.log(response.data);
       console.log(response.data.quotes[0].symbol)
-      props.setCompanySymbol(response.data.quotes[0].symbol)
+      console.log(response.data.quotes[0].shortname)
+      props.setCompanySymbol(response.data.quotes[0].symbol, response.data.quotes[0].shortname)
     }).catch(function (error) {
+      alert("Please enter a valid company name")
       console.error(error);
     });
 
